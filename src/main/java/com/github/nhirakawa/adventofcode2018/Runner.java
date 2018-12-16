98ad5d26779fabcc3c9bb21b44c48efd07a99d91
Nick Hirakawa <nh0815@gmail.com>
2018-12-16 16:56:48 -0500

    Day 3

---
+++ src/main/java/com/github/nhirakawa/adventofcode2018/Runner.java
@@ -0,0 +1,28 @@
+package com.github.nhirakawa.adventofcode2018;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.nhirakawa.adventofcode2018.dayone.DayOne;
+import com.github.nhirakawa.adventofcode2018.daythree.DayThree;
+import com.github.nhirakawa.adventofcode2018.daytwo.DayTwo;
+
+public class Runner {
+
+  private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+
+  public static void main(String... args) {
+    DayOne dayOne = new DayOne();
+    LOG.info("[Day 1] {}", dayOne.doPartOne());
+    LOG.info("[Day 1] {}", dayOne.doPartTwo());
+
+    DayTwo dayTwo = new DayTwo();
+    LOG.info("[Day 2] {}", dayTwo.doPartOne());
+    LOG.info("[Day 2] {}", dayTwo.doPartTwo());
+
+    DayThree dayThree = new DayThree();
+    LOG.info("[Day 3] {}", dayThree.doPartOne());
+    LOG.info("[Day 3] {}", dayThree.doPartTwo());
+  }
+
+}
