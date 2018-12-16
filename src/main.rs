840d30d66c85f3d2112df89f3657af20fe4beb57
Nick Hirakawa <nickhirakawa+github@gmail.com>
2018-12-16 13:19:21 -0500

    Day 1 (#1)
    * Part 1
    
    * Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+mod dayone;
+
+fn main() -> io::Result<()> {
+    dayone::dayone::do_part_one()?;
+    dayone::dayone::do_part_two()?;
+
+    Ok(())
 }
