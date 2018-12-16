ee333b5956e755055ba0faacce4c90d8fce34a28
Nick Hirakawa <nh0815@gmail.com>
2018-12-16 15:23:04 -0500

    Part 2

--- src/daytwo.rs
+++ src/daytwo.rs
@@ -4,11 +4,10 @@ use std::collections::HashSet;
 use multiset::HashMultiSet;
 use std::fs::File;
 use std::io::{self, BufReader, BufRead};
+use itertools::Itertools;
 
 pub fn do_part_one() -> io::Result<()> {
-    let reader = read_input()?;
-
-    let lines_vec = Vec::from_iter(reader.lines().flat_map(|s| s));
+    let lines_vec = read_input()?;
 
     let mut twos = HashSet::new();
     let mut threes = HashSet::new();
@@ -29,12 +28,40 @@ pub fn do_part_one() -> io::Result<()> {
         }
     }
 
-    println!("[ day2-1] checksum {}", twos.len() * threes.len());
+    println!("[day 2-1] checksum {}", twos.len() * threes.len());
 
     Ok(())
 }
 
-fn read_input() -> io::Result<BufReader<File>> {
+pub fn do_part_two() -> io::Result<()> {
+    let lines_vec = read_input()?;
+
+    let products = lines_vec.iter().cartesian_product(lines_vec.iter());
+
+    for (str1, str2) in products {
+        let str_set_1: Vec<char> = Vec::from_iter(str1.chars());
+        let str_set_2: Vec<char> = Vec::from_iter(str2.chars());
+
+        let zip = str_set_1.iter().zip(str_set_2.iter());
+
+        let mut common_chars = Vec::new();
+        for (char1, char2) in zip {
+            if char1 == char2 {
+                common_chars.push(char1);
+            }
+        }
+
+        if common_chars.len() == str1.len() - 1 {
+            let common_string: String = common_chars.into_iter().collect();
+            println!("[day 2-2] common characters {}",common_string);
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn read_input() -> io::Result<Vec<String>> {
     let f = File::open("inputs/day-two-input.txt")?;
-    Ok(BufReader::new(f))
+    Ok(Vec::from_iter(BufReader::new(f).lines().flat_map(|s| s)))
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() -> io::Result<()> {
     dayone::dayone::do_part_two()?;
 
     daytwo::do_part_one()?;
+    daytwo::do_part_two()?;
 
     Ok(())
 }
