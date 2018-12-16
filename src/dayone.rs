ca172007388abefe909b1d98f17a12270dfcc1e9
Nick Hirakawa <nickhirakawa@gmail.com>
2018-12-16 13:03:48 -0500

    Part 2

--- src/dayone.rs
+++ src/dayone.rs
@@ -4,22 +4,41 @@ pub mod dayone {
     use std::fs::File;
     use std::io::prelude::*;
     use std::io::{self, BufReader};
+    use std::iter::FromIterator;
 
     pub fn do_part_one() -> io::Result<()> {
-        let f = File::open("day-1-input.txt")?;
-        let f = BufReader::new(f);
+        let f = read_input()?;
 
         let mut sum = 0;
         for line in f.lines() {
             sum += line.unwrap().parse::<i32>().unwrap();
         }
 
-        println!("{}", sum);
+        println!("sum of file is {}", sum);
 
         Ok(())
     }
 
-    pub fn do_part_two() -> io::Result<()> {}
+    pub fn do_part_two() -> io::Result<()> {
+        let f = read_input()?;
+
+        let num_vec = Vec::from_iter(f.lines().flat_map(|s| s).flat_map(|s| s.parse::<i32>()));
+        let cycled = num_vec.iter().cycle();
+
+        let mut sum = 0;
+        let mut already_seen = HashSet::new();
+        for num in cycled {
+            sum += num;
+            let is_unique = !already_seen.insert(sum);
+            if is_unique {
+                break;
+            }
+        }
+
+        println!("first duplicate sum is {}", sum);
+
+        Ok(())
+    }
 
     fn read_input() -> std::result::Result<BufReader<File>, io::Error> {
         let f = File::open("day-1-input.txt")?;
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use std::io;
 mod dayone;
 
 fn main() -> io::Result<()> {
-    dayone::dayone::do_day_one()
+    dayone::dayone::do_part_one()?;
+    dayone::dayone::do_part_two()?;
+
+    Ok(())
 }
