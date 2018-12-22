144935fb5d61af0c3c985c532c7e6dc8d47f7b96
Nick Hirakawa <nickhirakawa+github@gmail.com>
2018-12-22 01:56:53 -0500

    Day 5 (#7)

--- src/main/java/com/github/nhirakawa/adventofcode2018/Util.java
+++ src/main/java/com/github/nhirakawa/adventofcode2018/Util.java
@@ -2,10 +2,19 @@ package com.github.nhirakawa.adventofcode2018;
 
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.google.common.collect.Multiset;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 public final class Util {
+
+  private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool(
+      new ThreadFactoryBuilder()
+          .setNameFormat("advent-of-code")
+          .build()
+  );
 
   private Util() {}
 
@@ -13,6 +22,10 @@ public final class Util {
     return multiset.entrySet().stream()
         .max(Comparator.comparing(Multiset.Entry::getCount))
         .map(Multiset.Entry::getElement);
+  }
+
+  public static ExecutorService executor() {
+    return EXECUTOR;
   }
 
 }
